3ce5ac1d39064c603429fc2382d838ba7e9ea92a
Eliezer Nogueira <eliezernogueira.rj@gmail.com>
2025-04-24 11:13:35 -0300

    Filtro de Execuções em ERRO, startedAt = null

--- clearExecutionsErrorN8N.sql
+++ clearExecutionsErrorN8N.sql
@@ -1,8 +1,12 @@
+-- Deleta os dados relacionados às execuções com erro/crash e startedAt nulo
 DELETE FROM public.execution_data
 WHERE "executionId" IN (
     SELECT id FROM public.execution_entity 
     WHERE status IN ('error', 'crashed')
+      AND "startedAt" IS NULL
 );
 
+-- Depois de apagar os dados relacionados, deleta as execuções em si
 DELETE FROM public.execution_entity
-WHERE status IN ('error', 'crashed');
+WHERE status IN ('error', 'crashed')
+  AND "startedAt" IS NULL;
